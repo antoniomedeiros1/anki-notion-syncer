c096fdce49c7a96d2e4b3f1436aa43f81487b643
Antonio <antonio.medeiros@estudante.ufjf.br>
2023-04-29 17:53:50 -0300

    initializing cobra project with cobra generator

--- README.md
+++
@@ -1,2 +0,0 @@
-# anki-notion-syncer
-Simple automation for creating decks in Anki with Notion
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 Antonio Medeiros <antonio.medeiros@estudante.ufjf.br>
+
+*/
+package main
+
+import "github.com/antoniomedeiros1/anki-notion-syncer/cmd"
+
+func main() {
+	cmd.Execute()
+}
